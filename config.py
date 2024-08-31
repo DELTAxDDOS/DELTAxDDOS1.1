c4aa9bbae2bcf8a606b082c18101b0ce03ae3063
DELTAxDDOS <idkhu102@gmail.com>
2024-08-31 18:36:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY The_Fiend102
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("6319750205:AAElwPV4-nrC8VKbKma5OvzmD_a0HrUo3ZA")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("The_Fiend102")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("709227486")
+
+
+
+
+
+
+
+  
