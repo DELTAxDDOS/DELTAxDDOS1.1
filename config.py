4a6c90cfcfd98c3d6fbc9940f0fbe38c83723ab6
DELTAxDDOS <idkhu102@gmail.com>
2024-09-05 14:15:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY The_Fiend102
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("6319750205:AAElwPV4-nrC8VKbKma5OvzmD_a0HrUo3ZA")
+BOT_TOKEN=("YOUR BOT TOKEN")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("The_Fiend102")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("709227486")
+ADMIN_IDS=("TELEGRAM ID")
 
 
 
